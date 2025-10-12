ad303af6be5c0d3b23762e107ca0e75febecb3d6
JoseRT <joseryutoari@gmail.com>
2025-10-12 17:15:01 +0700

    added GameAccount model referencing tournaments

---
+++ game_account/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GameAccountConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'game_account'
---
+++ game_account/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+from django.conf import settings
+
+class GameAccount(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    game = models.ForeignKey('tournaments.Game', on_delete=models.PROTECT) # reference ke model Game di app tournaments
+    ingame_name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        # constraint utk mencegah dua GameAccount yg beda punya kombinasi game dan ingame_name yg sama
+        constraints = [
+            models.UniqueConstraint(fields=['game', 'ingame_name'], name='unique_game_ingame_if_active', condition=models.Q(active=True))
+        ]
+
+    def __str__(self):
+        return f"{self.ingame_name} ({self.game})"
