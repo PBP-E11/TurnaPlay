108663bb4f3e4e509daa89de7dd7d3b60d2be35b
Wahyuga <tawahyuga17@gmail.com>
2025-10-22 13:41:11 +0700

    Integrate UserAcccount with Django auth

--- turnaplay/settings.py
+++ turnaplay/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tournaments',
-    'game_account'
+    'game_account',
     'user_account',
 ]
 
@@ -153,3 +153,9 @@ STATIC_ROOT = BASE_DIR / 'static_root'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'user_account.UserAccount'
+
+# Login/Logout URLs
+LOGIN_URL = '/login/'
